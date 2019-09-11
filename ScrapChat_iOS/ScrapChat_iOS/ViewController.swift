71d014fd75c8d41f387d613901cd50c67c0b308a
Jaiden Chicote <jedezzio@gmail.com>
2019-09-12 01:00:23 +1000

    Adjusted Constraints + Animated buttons
    - Edit on main controller's code
    
    BUGs:
    - During first load the expandable buttons are not centering to callButton and remain at their default positions.

--- ScrapChat_iOS/ScrapChat_iOS/ViewController.swift
+++ ScrapChat_iOS/ScrapChat_iOS/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var asOne: UIButton!
+    @IBOutlet weak var asGroup: UIButton!
+    @IBOutlet weak var callButton: UIButton!
+    
+    var isExpanded = false
+    var oneButtonCenter: CGPoint!
+    var groupButtonCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        oneButtonCenter = asOne.center
+        groupButtonCenter = asGroup.center
+        
+        asOne.center = callButton.center
+        print(asOne.center)
+        asGroup.center = callButton.center
+        print(asGroup.center)
+
     }
 
-
+    @IBAction func expandClicked(_ sender: UIButton) {
+        if isExpanded == false {
+            UIView.animate(withDuration: 0.3, animations: {
+                sender.backgroundColor = UIColor(red: 5/255, green: 194/255, blue: 180/255, alpha: 1)
+                self.asOne.center = self.oneButtonCenter
+                self.asGroup.center = self.groupButtonCenter
+            })
+            isExpanded = true
+        } else if isExpanded == true {
+            UIView.animate(withDuration: 0.3, animations: {
+                sender.backgroundColor = UIColor(red: 0, green: 255/255, blue: 138/255, alpha: 1)
+                self.asOne.center = self.callButton.center
+                self.asGroup.center = self.callButton.center
+            })
+            isExpanded = false
+        }
+    }
+    
 }
 
