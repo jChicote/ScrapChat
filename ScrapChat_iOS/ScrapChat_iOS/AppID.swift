6a02e18a99a7c0e820fa4b73b7493be9d88bbd11
Jaiden Chicote <jedezzio@gmail.com>
2019-09-21 23:40:32 +1000

    Call feature upload test
    Test upload to v2 containing call feature

---
+++ ScrapChat_iOS/ScrapChat_iOS/AppID.swift
@@ -0,0 +1,11 @@
+//
+//  AppID.swift
+//  ScrapChat_iOS
+//
+//  Created by Jaiden Chicote on 21/9/19.
+//  Copyright © 2019 Dream Team. All rights reserved.
+//
+
+let AppID: String = "7733af5caa3948f1ba89cb4d8f08bf68"
+
+let Token: String? = "0067733af5caa3948f1ba89cb4d8f08bf68IACeN4zbqPEAtPEPTMhiiKJuSbpQRvUa+071sZp2M/rrMr3OOjoAAAAAEADCDWTFbi2GXQEAAQBuLYZd"
